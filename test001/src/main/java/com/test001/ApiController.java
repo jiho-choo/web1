4d120008d910597fa56529b00106c6322473aecc
jiho-choo <asuraho@gmail.com>
2021-02-17 23:48:13 +0900

    spring restapi test001

--- test001/src/main/java/com/test001/ApiController.java
+++ test001/src/main/java/com/test001/ApiController.java
@@ -2,18 +2,23 @@ package com.test001;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ApiController {
+
 
     @RequestMapping(value = "/api/test", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
     public String getApiTest() {
 
-        return "{\"result\", \"ok\"}";
+        return "{\"result\", \"qweqwe\"}";
+    }
+
+    @RequestMapping(value = "/api/test2", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public String getApiTest2() {
+
+        return "{\"result\", \"qqqqqqqq\"}";
     }
 }
